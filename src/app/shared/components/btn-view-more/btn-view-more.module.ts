a561bba91c55ec3915d4f65425985285366d4680
Home <home@AGM-606503.local>
2022-03-21 21:48:41 -0300

    [btn-view-more] - create styles

--- src/app/shared/components/btn-view-more/btn-view-more.module.ts
+++ src/app/shared/components/btn-view-more/btn-view-more.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BtnViewMoreComponent } from './btn-view-more.component';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [ BtnViewMoreComponent ],
   imports: [
     CommonModule,
-    MatIconModule,
-    MatProgressSpinnerModule
+    MatIconModule
   ],
   exports: [ BtnViewMoreComponent ]
 })
