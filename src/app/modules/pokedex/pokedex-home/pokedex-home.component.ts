78bc55b7680e41b48e9abaf5523b6ed6ef359bd3
Home <home@AGM-606503.local>
2022-04-03 21:09:51 -0300

    search field

--- src/app/modules/pokedex/pokedex-home/pokedex-home.component.ts
+++ src/app/modules/pokedex/pokedex-home/pokedex-home.component.ts
@@ -38,8 +38,9 @@ export class PokedexHomeComponent implements OnInit, OnDestroy {
   receiveSearch(value: string): void {
     this.searchValue = value;
     this.unsubscribePokemons();
+    if(typeof(value) === "string")
       this.pokemonsGet$ = value ?
-      this.pokemonService.getPokemonBySearch(value).subscribe() :
+      this.pokemonService.getPokemonBySearch(this.searchValue).subscribe() :
       this.pokemonService.getPokemons(true).subscribe();
   }
 
